01bee0fa602c9115d9e28cbb42a3b8df03ebe2d7
Ondřej Šibrava <ondrej.sibrava@varhall.cz>
2018-02-16 07:19:35 +0100

    "datetime" validation rule support

--- src/utils/transformation/Transformator.php
+++ src/utils/transformation/Transformator.php
@@ -12,20 +12,20 @@ use Varhall\Restino\Utils\Configuration;
 class Transformator
 {
     private static $instance = NULL;
-    
+
     private $transformators = NULL;
-    
+
     private $defaults       = [ 'trim', 'number', 'date' ];
-    
+
     // singleton factory method
     public static function __callStatic($name, $arguments)
     {
         if (!self::$instance)
             self::$instance = new static();
-        
+
         return call_user_func_array([self::$instance, $name], $arguments);
     }
-    
+
     private function __construct()
     {
         $this->transformators = [
@@ -44,12 +44,13 @@ class Transformator
             'boolean'       => new Transformators\Boolean(),
 
             'date'          => new Transformators\Date(),
+            'datetime'      => new Transformators\Date(),
         ];
     }
 
 
     /////////////////////////// Fictive static methods //////////////////////////////
-    
+
     private function addTransformator($name, Transformators\ITransformator $transformate)
     {
         $this->transformators[$name] = $transformate;
@@ -59,17 +60,17 @@ class Transformator
     {
         return [ 'trim' ];
     }
-    
+
     //////////////////////////////// Transformation ////////////////////////////////
-    
+
     private function transformate(array $data, array $rules, $section = NULL)
     {
         if (!empty($section))
             $rules = Configuration::extractSection($rules, $section);
-        
+
         return $this->transformateData($data, $rules);
     }
-    
+
     private function transformateData(array $data, array $rules)
     {
         foreach ($data as $key => $value) {
@@ -79,14 +80,14 @@ class Transformator
         }
         return $data;
     }
-    
+
     private function transformField($value, array $rules)
     {
         foreach ($rules as $rule) {
             if (isset($this->transformators[$rule]))
                 $value = $this->transformators[$rule]->apply($value);
         }
-        
+
         return $value;
     }
 }
--- src/utils/validation/Validator.php
+++ src/utils/validation/Validator.php
@@ -12,45 +12,46 @@ use Varhall\Restino\Utils\Configuration;
 class Validator
 {
     private static $instance = NULL;
-    
+
     private $rules = NULL;
-    
+
     // singleton factory method
     public static function __callStatic($name, $arguments)
     {
         if (!self::$instance)
             self::$instance = new static();
-        
+
         return call_user_func_array([self::$instance, $name], $arguments);
     }
-    
+
     private function __construct()
     {
         $this->rules = [
             'required'  => new Rules\Required(),
             'regex'     => new Rules\Regex(),
             'date'      => new Rules\Date(),
+            'datetime'  => new Rules\Date(),
             'enum'      => new Rules\Enum()
         ];
     }
-    
+
     public function addRule($name, Rules\IRule $rule)
     {
         $this->rules[$name] = $rule;
-    }     
-    
-    
-    
+    }
+
+
+
     //////////////////////////////// VALIDATION ////////////////////////////////
-    
-    
+
+
     /**
      * Provede validaci vstupnich dat. Jako validacni volby je mozne pouzit podle https://doc.nette.org/cs/2.4/validators
-     * 
+     *
      * <b>pridana pravidla:</b>
      *  - required  = pole musi byt obsazeno a nesmi byt prazdne<br>
      *  - regex     = stejne jako pattern<br>
-     * 
+     *
      * <b>tvar pravidel:</b><br>
      * [<br>
      *      nazev_pole_1 => 'pravidlo',<br>
@@ -58,7 +59,7 @@ class Validator
      *      nazev_pole_3 => ['pravidlo1', 'pravidlo2'],<br>
      *      nazev_pole_4 => ['pravidlo1, 'pravidlo2|pravidlo3']<br>
      * ]<br>
-     * 
+     *
      * @param array $data Vstupni data
      * @param array $rules Pole pravidel v pozadovanem tvaru
      * @return array Asociativni pole chyb ve tvaru [nazev_pole => 'chyba']
@@ -67,102 +68,50 @@ class Validator
     {
         if (!empty($section))
             $rules = Configuration::extractSection($rules, $section);
-        
+
         return $this->validateData($data, $rules);
     }
-    
+
     private function validateData(array &$data, array $rules)
     {
         $errors = [];
         foreach ($rules as $property => $propRules) {
             $propRules = Configuration::splitRule($propRules);
-            
+
             // ignore property if isn't in source and add error if is required
             if (!isset($data[$property]) && in_array('required', $propRules))
                 $errors[$property] = 'Field is required';
-                    
+
             if (!isset($data[$property]))
                 continue;
-             
+
             // process each rule
             try {
                 $this->validateField($data[$property], $propRules);
-                        
+
             } catch (\Nette\Utils\AssertionException $ex) {
                 $errors[$property] = $ex->getMessage();
             }
         }
-        
+
         return $errors;
     }
-    
+
     /**
      * Provede kontrolu hodnoty a v pripade chyby vyhodi vyjimku
-     * 
+     *
      * @param mixed $value Kontrolovana hodnota
      * @param array $rules Rozparsovane pole pravidel
      * @throws \Nette\Utils\AssertionException V pripade, ze hodnota nevyhovuje nejakemu pravidlu
      */
     private function validateField(&$value, array $rules)
     {
-        /*$customValidators = [
-            'required'  => function($value, $expected) {
-                // the rule does nothing because sometimes is necessary to send empty value (0, '', false, ...)
-            },
-            'regex'     => function($value, $expected) {
-                \Nette\Utils\Validators::assert($value, 'pattern:' . $expected);
-            },
-            'date'      => function(&$value, $expected) {
-                try {
-                    $value = \Nette\Utils\DateTime::from(strtotime($value));
-                    
-                } catch (\Exception $ex) {
-                    throw new \Nette\Utils\AssertionException('Value ' . $value . ' is not correct date');
-                }
-            },
-            'enum'      => function($value, $expected) {
-                $enum = array_map('trim', explode(',', $expected));
-                if (!in_array($value, $enum))
-                    throw new \Nette\Utils\AssertionException('Field not match enum [' . $expected . ']');
-            }
-        ];*/
-        
-        /*$transformers = [
-            'int'       => function($value) { 
-                return \Nette\Utils\Validators::isNumericInt($value) ? intval($value) : $value; 
-            },
-            'integer'   => function($value) { 
-                return \Nette\Utils\Validators::isNumericInt($value) ? intval($value) : $value; 
-            },
-            'float'     => function($value) { 
-                $value = str_replace(',', '.', $value);
-                
-                return \Nette\Utils\Validators::isNumeric($value) ? floatval($value) : $value; 
-            },
-            'number'    => function($value) { 
-                $value = str_replace(',', '.', $value);
-
-                
-                if (\Nette\Utils\Validators::isNumericInt($value)) 
-                    return intval($value);
-                
-                else if (\Nette\Utils\Validators::isNumeric($value))
-                    return floatval($value);
-                
-                return $value;
-            }
-        ];*/
-        
         foreach ($rules as $rule) {
             $parts = explode(':', $rule, 2);
-            
+
             $type = $parts[0];
             $args = (count($parts) > 1) ? $parts[1] : NULL;
 
-            // pokud existuje transformacni pravidlo, provede jej
-            if (isset($transformers[$type]) && is_callable($transformers[$type]))
-                $value = $transformers[$type]($value);
-            
             // zkontroluje pole a v pripade chyby vyhodi vyjimku
             if (isset($this->rules[$type])) {
                 $this->rules[$type]->apply($value, $args);
