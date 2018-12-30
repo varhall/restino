0af2ee34ab96aecd426621f614822c2c5d93c037
Ondřej Šibrava <ondrej.sibrava@varhall.cz>
2018-12-30 14:41:17 +0100

    utilino dependency

--- src/utils/FileUtils.php
+++ src/utils/FileUtils.php
@@ -29,59 +29,12 @@ class FileUtils
             if (!isset($file['name']))
                 $file['name'] = NULL;
 
-            $result[] = self::fromBase64($file['data'], $file['name']);
+            $result[] = \Varhall\Utilino\Files\FileUtils::fromBase64($file['data'], $file['name']);
         }
 
         if ($removePropety)
             unset($files[$property]);
 
         return $result;
-    }
-
-    public static function fromBase64($base64, $filename = NULL)
-    {
-        $data = self::parseBase64($base64);
-
-        if (!$data)
-            throw new \Nette\InvalidArgumentException('Given string is not valid base64 file');
-
-        return self::fromBinary(base64_decode($data['content']), $filename);
-    }
-
-    public static function fromBinary($data, $filename = NULL)
-    {
-        if (empty($filename))
-            $filename = 'unknown_file';
-
-        $tmp = tmpfile();
-        $tmpName = stream_get_meta_data($tmp)['uri'];
-
-        fwrite($tmp, $data);
-        $_FILES['file_' . \Nette\Utils\Strings::webalize($filename)] = $tmp;
-
-        return new \Nette\Http\FileUpload([
-            'name'      => $filename,
-            'tmp_name'  => $tmpName,
-            'type'      => finfo_file(finfo_open(FILEINFO_MIME_TYPE), $tmpName),      // automatically retrieved
-            'size'      => filesize($tmpName),
-            'error'     => 0
-        ]);
-    }
-
-    protected static function parseBase64($data)
-    {
-        // validates Data URI scheme (https://en.wikipedia.org/wiki/Data_URI_scheme)
-        if (!is_string($data) || !preg_match('/^data:.+;.+,.+$/i', substr($data, 0, 200)))
-            return NULL;
-
-        list($head, $content) = explode(',', $data, 2);
-
-        $head = str_replace('data:', '', $head);
-        list($type) = explode(';', $head);
-
-        return [
-            'type'      => $type,
-            'content'   => $content
-        ];
     }
 }
